ca709e86ac9ad2b22d5dcfb017d5f3f8f0654d63
Peter Houghton <pete@investigatingsoftware.co.uk>
2022-03-10 16:24:23 +0000

    Add initial turning code

---
+++ main.py
@@ -0,0 +1,50 @@
+import PicoMotorDriver
+import time
+
+board = PicoMotorDriver.KitronikPicoMotor()
+
+motor_left = 1
+motor_right = 2
+
+def turn(hand_direction="right", speed=50, hardness=50):
+    # hardness between 100 and -90
+    # 100 is max with tracks going fully in opposite direction (hard turn)
+    # -90 is close to minimum with tracks going at 100 and 90 (slow turn)
+    
+    lead_track_direction = "f"
+    trailing_track_direction = "f"
+    
+    lead_track_speed = speed
+    trailing_track_speed = int((-hardness/100)*speed)
+    
+    if trailing_track_speed < 0:
+        trailing_track_direction = "r"
+    
+    trailing_track_speed = abs(trailing_track_speed)
+    
+    if hand_direction == "left":
+        board.motorOn(motor_left, trailing_track_direction, trailing_track_speed)
+        board.motorOn(motor_right, lead_track_direction, lead_track_speed)
+    
+    if hand_direction == "right":
+        board.motorOn(motor_left, lead_track_direction, lead_track_speed)
+        board.motorOn(motor_right, trailing_track_direction, trailing_track_speed)
+
+def dead_stop():
+    board.motorOff(motor_left)
+    board.motorOff(motor_right)
+
+time.sleep(3)
+turn(hand_direction="right", speed=100, hardness=90)
+time.sleep(2)
+dead_stop()
+turn(hand_direction="left", speed=100, hardness=90)
+time.sleep(2)
+dead_stop()
+time.sleep(3)
+turn(hand_direction="right", speed=100, hardness=-50)
+time.sleep(2)
+dead_stop()
+turn(hand_direction="left", speed=100, hardness=-50)
+time.sleep(2)
+dead_stop()
